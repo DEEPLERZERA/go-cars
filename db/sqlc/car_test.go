8582ebca7957febb5b6529f75dba416e073a0207
Daniel <dani2014games@gmail.com>
2023-10-17 01:51:32 -0300

    [GM-04] feat: Adding Unit tests functions in the project

---
+++ db/sqlc/car_test.go
@@ -0,0 +1,96 @@
+package db 
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomCar(t *testing.T) Car {
+	arg := CreateCarParams{
+		Name:  "BMW X5",
+		Price:  60000,
+		Brand:  "BMW",
+	}
+
+	car, err := testQueries.CreateCar(context.Background(), arg)
+
+
+	require.NoError(t, err)
+	require.NotEmpty(t, car)
+
+	require.Equal(t, arg.Name, car.Name)
+	require.Equal(t, arg.Price, car.Price)
+	require.Equal(t, arg.Brand, car.Brand)
+
+	require.NotEmpty(t, car.ID)
+	require.NotEmpty(t, car.CreatedAt)
+
+	return car
+}
+
+func TestCreateCar(t *testing.T) {
+	createRandomCar(t)
+}
+
+func TestGetCar(t *testing.T) {
+	carRandomCreated := createRandomCar(t)
+	carFinded, err := testQueries.GetCar(context.Background(), carRandomCreated.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, carRandomCreated)
+	require.NotEmpty(t, carFinded)
+
+	require.Equal(t, carRandomCreated.ID, carFinded.ID)
+	require.Equal(t, carRandomCreated.Name, carFinded.Name)
+	require.Equal(t, carRandomCreated.Price, carFinded.Price)
+	require.Equal(t, carRandomCreated.Brand, carFinded.Brand)
+	require.Equal(t, carRandomCreated.CreatedAt, carFinded.CreatedAt)
+}
+
+func TestDeleteCar(t *testing.T) {
+	carRandomCreated := createRandomCar(t)
+	err := testQueries.DeleteCar(context.Background(), carRandomCreated.ID)
+
+	require.NoError(t, err)
+}
+
+func TestUpdateCar(t *testing.T) {
+	carRandomCreated := createRandomCar(t)
+
+	arg := UpdateCarParams{
+		ID: carRandomCreated.ID,
+		Name: "BMW X6",
+		Price:  70000,
+		Brand:  "BMW",
+	}
+
+	carUpdated, err := testQueries.UpdateCar(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, carUpdated)
+
+	require.Equal(t, carRandomCreated.ID, carUpdated.ID)
+	require.Equal(t, arg.Name, carUpdated.Name)
+	require.Equal(t, arg.Price, carUpdated.Price)
+	require.Equal(t, arg.Brand, carUpdated.Brand)
+	require.Equal(t, carRandomCreated.CreatedAt, carUpdated.CreatedAt)
+}
+
+func TestGetCars(t *testing.T) {
+	carRandomCreated := createRandomCar(t)
+	carsFinded, err := testQueries.GetCars(context.Background())
+
+	require.NoError(t, err)
+	require.NotEmpty(t, carRandomCreated)
+	require.NotEmpty(t, carsFinded)
+
+	for _, car := range carsFinded {
+		require.NotEmpty(t, car.ID)
+		require.NotEmpty(t, car.Name)
+		require.NotEmpty(t, car.Price)
+		require.NotEmpty(t, car.Brand)
+		require.NotEmpty(t, car.CreatedAt)
+	}
+}
